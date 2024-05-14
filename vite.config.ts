f28e11c624879c7c0fb9ba1a288d1813850ae696
kheeyaa <kheeyaa@icloud.com>
2024-05-14 23:01:37 +0900

    #6 feat: vite proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.cli-meet.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: true,
+      },
+    },
+  },
 })

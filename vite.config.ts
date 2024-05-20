0b30a72ec201bb92d723b54300024ca3b51daf3b
dev_hee <kheeyaa888@gmail.com>
2024-05-20 21:54:52 +0900

    #6 fix: Prod에서 proxy 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.cli-meet.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: true,
-      },
-    },
-  },
-})
+export default ({ mode }: { mode: string }) => {
+  const isDevelop = mode === 'development'
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [react()],
+    server: isDevelop
+      ? {
+          proxy: {
+            '/api': {
+              target: 'https://api.cli-meet.com',
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ''),
+              secure: true,
+            },
+          },
+        }
+      : undefined,
+  })
+}

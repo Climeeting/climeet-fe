66b8b462b971426145d53b5f68fa5790e5d8973a
dev_hee <kheeyaa888@gmail.com>
2024-05-20 22:14:15 +0900

    #6 fix: vite.config.ts 수정
    - https://vitejs.dev/config/#using-environment-variables-in-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const isDevelop = mode === 'development'
+  const env = loadEnv(mode, process.cwd(), '')
+  const isDevelop = env.VITE_MODE === 'development'
+
   return {
     plugins: [react()],
     server: isDevelop
@@ -20,3 +22,24 @@ export default defineConfig(({ mode }) => {
       : undefined,
   }
 })
+// export default ({ mode }: { mode: string }) => {
+//   const isDevelop = mode === 'development'
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+//   console.log({ isDevelop })
+
+//   return defineConfig({
+//     plugins: [react()],
+//     server: isDevelop
+//       ? {
+//           proxy: {
+//             '/api': {
+//               target: 'https://api.cli-meet.com',
+//               changeOrigin: true,
+//               rewrite: (path) => path.replace(/^\/api/, ''),
+//               secure: true,
+//             },
+//           },
+//         }
+//       : undefined,
+//   })
+// }

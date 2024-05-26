edaf1c30e3823c578eee57e0d9816312139cf6d4
dev_hee <kheeyaa888@gmail.com>
2024-05-26 18:06:01 +0900

    #14 config: 패키지 설치 및 개발 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
+      svgr(),
       VitePWA({
         strategies: 'injectManifest',
         srcDir: 'src',

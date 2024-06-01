48cf8321b382f53adfa50ec61fe5f0452fe32cef
JunYoung Kim <twinjyjh2@gmail.com>
2024-06-01 14:58:28 +0900

    #15 feat: CragForm 암장 이름 입력 기능 추가

--- src/pages/PartySurveyForm/components/CragForm.tsx
+++ src/pages/PartySurveyForm/components/CragForm.tsx
@@ -1,5 +1,6 @@
 import styles from './CragForm.module.scss'
 import { SurveyPartyFormData } from '../PartySurveyFormPage.tsx'
+import { useState } from 'react'
 
 type CragFormProps = {
   onNext: () => void
@@ -7,7 +8,13 @@ type CragFormProps = {
   updateFormData: (key: keyof SurveyPartyFormData, value: string) => void
 }
 
-export function CragForm({ onNext }: CragFormProps) {
+export function CragForm({ onNext, formData, updateFormData }: CragFormProps) {
+  const [value, setValue] = useState(formData.cragName)
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -16,10 +23,21 @@ export function CragForm({ onNext }: CragFormProps) {
           <br />
           암장을 알려주세요.
         </h2>
-        <input className={styles.input} placeholder={'암장 이름을 입력해주세요.'} />
+        <input
+          value={value}
+          onChange={handleValueChange}
+          className={styles.input}
+          placeholder={'암장 이름을 입력해주세요.'}
+        />
       </div>
       <div className={styles.footer}>
-        <button className={styles.nextBtn} onClick={() => onNext('더클 사당')}>
+        <button
+          className={styles.nextBtn}
+          onClick={() => {
+            updateFormData('cragName', value)
+            onNext()
+          }}
+        >
           다음
         </button>
       </div>

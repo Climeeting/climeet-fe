db393604fc70192826012861b521813421966ee8
dev_hee <kheeyaa888@gmail.com>
2024-06-06 18:01:32 +0900

    #17 feat: 필터 진입 버튼 생성

---
+++ src/pages/Home/components/FilterButton.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren } from 'react'
+import styles from './FilterButton.module.scss'
+import Icon from '../../../components/Icon/Icon'
+
+type props = {
+  count?: number
+  active?: boolean
+  onClick?: () => void
+  onClickIcon?: () => void
+} & React.RefAttributes<HTMLButtonElement>
+
+export default function FilterButton({
+  children,
+  active,
+  onClick,
+  onClickIcon,
+  ...rest
+}: PropsWithChildren<props>) {
+  return (
+    <button
+      onClick={onClick}
+      className={styles.Button}
+      data-state={active ? 'active' : ''}
+      {...rest}
+    >
+      {children}
+      <div onClick={onClickIcon}>
+        {active ? <Icon icon="Delete" size="12" /> : <Icon icon="ArrowDown" size="12" />}
+      </div>
+    </button>
+  )
+}

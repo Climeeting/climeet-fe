33a70ce049600be1b694fde068f8ddc098246ec0
JunYoung Kim <twinjyjh2@gmail.com>
2024-06-08 00:12:39 +0900

    #15 feat: 파티 조건 페이지 아코디언 적용

--- src/pages/PartySurveyForm/components/PartyConditionForm.tsx
+++ src/pages/PartySurveyForm/components/PartyConditionForm.tsx
@@ -1,6 +1,8 @@
 import styles from './PartyConditionForm.module.scss'
 import { PartySurveyFormData } from '../PartySurveyFormPage.tsx'
 import { RadioButtonGroup } from '../../../components/RadioButtonGroup.tsx'
+import { AccordionContent, AccordionTrigger } from '../../../components/Accordion.tsx'
+import * as Accordion from '@radix-ui/react-accordion'
 
 type PartyConditionFormProps = {
   onNext: () => void
@@ -19,40 +21,42 @@ export function PartyConditionForm({ onNext, formData, updateFormData }: PartyCo
     <div className={styles.container}>
       <div>
         <h2 className={styles.title}>파티 조건을 선택해주세요.</h2>
-        <div className={styles.question}>
-          <h3 className={styles.questionTitle}>파티 인원 (본인 포함)</h3>
-          <input className={styles.input} placeholder={'0명'} />
-        </div>
-        <div className={styles.question}>
-          <h3 className={styles.questionTitle}>성별</h3>
-          <div className={styles.trigger}>
-            <div className={styles.triggerText}>{formData.gender}</div>
+        <Accordion.Root className="AccordionRoot" type="multiple" defaultValue={['item-1']}>
+          <div className={styles.question}>
+            <h3 className={styles.questionTitle}>파티 인원 (본인 포함)</h3>
+            <input className={styles.input} placeholder={'0명'} />
           </div>
-          <div className={styles.content}>
-            <RadioButtonGroup
-              list={genderList}
-              onValueChange={(value) => updateFormData('gender', value)}
-              defaultValue={formData.gender}
-            />
+          <div className={styles.question}>
+            <h3 className={styles.questionTitle}>성별</h3>
+            <Accordion.Item className="AccordionItem" value={`gender`}>
+              <AccordionTrigger>{formData.gender}</AccordionTrigger>
+              <AccordionContent>
+                <RadioButtonGroup
+                  list={genderList}
+                  onValueChange={(value) => updateFormData('gender', value)}
+                  defaultValue={formData.gender}
+                />
+              </AccordionContent>
+            </Accordion.Item>
           </div>
-        </div>
-        <div className={styles.question}>
-          <h3 className={styles.questionTitle}>종목</h3>
-          <div className={styles.trigger}>
-            <div className={styles.triggerText}>{formData.subject}</div>
+          <div className={styles.question}>
+            <h3 className={styles.questionTitle}>종목</h3>
+            <Accordion.Item className="AccordionItem" value={`subject`}>
+              <AccordionTrigger>{formData.subject}</AccordionTrigger>
+              <AccordionContent>
+                <RadioButtonGroup
+                  list={subjectList}
+                  onValueChange={(value) => updateFormData('subject', value)}
+                  defaultValue={formData.subject}
+                />
+              </AccordionContent>
+            </Accordion.Item>
           </div>
-          <div className={styles.content}>
-            <RadioButtonGroup
-              list={subjectList}
-              onValueChange={(value) => updateFormData('subject', value)}
-              defaultValue={formData.subject}
-            />
+          <div className={styles.question}>
+            <h3 className={styles.questionTitle}>실력</h3>
+            <input className={styles.input} placeholder={'상관없음'} />
           </div>
-        </div>
-        <div className={styles.question}>
-          <h3 className={styles.questionTitle}>실력</h3>
-          <input className={styles.input} placeholder={'상관없음'} />
-        </div>
+        </Accordion.Root>
       </div>
       <div className={styles.footer}>
         <button className={styles.nextBtn} onClick={onNext}>

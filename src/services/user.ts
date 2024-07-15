53365444645ffaf95fb88e7eeb848cb6bc88ca12
piggmme <kheeyaa888@gmail.com>
2024-07-15 16:28:36 +0900

    fix: 로그인 접근 제어 페이지에서 새로고침시 로그인 되어있음에도 로그인페이지로 리다이렉트되는 버그

--- src/services/user.ts
+++ src/services/user.ts
@@ -35,8 +35,25 @@ export const useMyProfile = () => {
 }
 
 export const useIsLogin = () => {
-  const { data, isError } = useMyProfile()
-  return !!data && !isError
+  return useQuery({
+    queryKey: USER_KEY,
+    queryFn: get_user_myProfile,
+    select: (data) => !!data,
+    // 1시간마다 새로고침
+    refetchInterval: 60 * 60 * 1000,
+    // 마운트시에 요청 보내지 않음
+    retryOnMount: false,
+    // 윈도우 포커스시에 새로고침하지 않음
+    refetchOnWindowFocus: false,
+
+    // 인증 만료시 재시도하지 않음
+    retry: (failureCount, error) => {
+      if (isAxiosError(error) && error.response && error.response.status === 403) {
+        return false
+      }
+      return failureCount < 3
+    },
+  })
 }
 
 export const MyProfileQuery = {

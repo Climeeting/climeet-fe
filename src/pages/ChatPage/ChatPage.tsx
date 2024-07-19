ede3a9d9d9173490d21a33e6de0372a8c3c3cc17
JunYoung Kim <twinjyjh2@gmail.com>
2024-07-19 23:26:39 +0900

    #101 feat: 참여중인 채팅이 없을 경우 표시되는 컴포넌트 생성

--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -3,28 +3,27 @@ import styles from './ChatPage.module.scss'
 import Avatar from '@/components/Avatar.tsx'
 import TopBar from '@/components/NavBar/TopBar.tsx'
 import BottomBar from '@/components/NavBar/BottomBar.tsx'
+import emptyChat from '@/assets/empty_chat.png'
+import { useState } from 'react'
+
+type ChatType = 'party' | 'personal'
 
 function ChatPage() {
+  const [chatType, setChatType] = useState<ChatType>('party')
+
   return (
     <div className={styles.Container}>
       <TopBar>
         <TopBar.Left back />
         <TopBar.Center>채팅</TopBar.Center>
       </TopBar>
-      <Tabs tabs={['파티', '개인']} />
-      <div className={styles.ChatList}>
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-        <ChatItem />
-      </div>
+      <Tabs
+        tabs={['파티', '개인']}
+        onClick={(tab) => {
+          setChatType(tab === '파티' ? 'party' : 'personal')
+        }}
+      />
+      {chatType === 'party' ? <EmptyChat /> : <ChatItem />}
       <BottomBar />
     </div>
   )
@@ -49,6 +48,20 @@ function ChatItem() {
         <div className={styles.LastChatTime}>오전 8:42</div>
         <div className={styles.Chip}>12</div>
       </div>
+    </div>
+  )
+}
+
+function EmptyChat() {
+  return (
+    <div className={styles.EmptyChat}>
+      <img src={emptyChat} alt={'empty-chat-img'} />
+      <div className={styles.EmptyChatText}>
+        참여중인 채팅이 없습니다.
+        <br />
+        파티에 참가하고 파티원과 대화를 나눠보세요!
+      </div>
+      <button className={styles.Button}>파티 목록 보기</button>
     </div>
   )
 }

3629c3829c4ce8bb7856d46e014e331aa9c2b87a
JunYoung Kim <twinjyjh2@gmail.com>
2024-07-21 23:58:50 +0900

    #101 feat: 채팅 인풋 클릭시 가상 영역이 스크롤되는 문제 해결

--- src/pages/ChatRoomPage/ChatRoomPage.tsx
+++ src/pages/ChatRoomPage/ChatRoomPage.tsx
@@ -2,33 +2,83 @@ import styles from './ChatRoomPage.module.scss'
 import TopBar from '@/components/NavBar/TopBar.tsx'
 import classNames from 'classnames'
 import Icon from '@/components/Icon/Icon.tsx'
-import { useState } from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 import SideSheet from '@/components/SideSheet.tsx'
 import ChatSidebar from '@/pages/ChatRoomPage/components/ChatSidebar.tsx'
 
 function ChatRoomPage() {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  // 가상 영역이 스크롤되는 문제를 해결 (viewport 이상으로 스크롤 안되도록 설정)
+  const handleViewportScroll = (e: Event) => {
+    const viewportScrollY = (e.target as HTMLElement).offsetTop
+    if (!wrapperRef.current) {
+      return
+    }
+    wrapperRef.current.style.transform = `translateY(${viewportScrollY}px)`
+  }
+
+  const setWrapperHeight = () => {
+    if (!wrapperRef.current) {
+      return
+    }
+
+    const nextHeight = visualViewport?.height ?? 0
+    wrapperRef.current.style.height = `${nextHeight}px`
+  }
+
+  const setContainerHeight = () => {
+    if (!containerRef.current) {
+      return
+    }
+
+    const nextHeight = (visualViewport?.height ?? 0) - 64
+    containerRef.current.style.height = `${nextHeight}px`
+  }
+
+  const setHeight = () => {
+    setWrapperHeight()
+    setContainerHeight()
+  }
+
+  useLayoutEffect(() => {
+    setHeight()
+    visualViewport?.addEventListener('resize', setHeight)
+    visualViewport?.addEventListener('scroll', handleViewportScroll)
+
+    return () => {
+      visualViewport?.removeEventListener('resize', setHeight)
+      visualViewport?.removeEventListener('scroll', handleViewportScroll)
+    }
+  }, [])
+
   return (
-    <div className={styles.Container}>
-      <TopBar>
-        <TopBar.Left back />
-        <TopBar.Center>친해지실 분 구해유</TopBar.Center>
-        <TopBar.Right close={false}>
-          <ChatRoomInfo />
-        </TopBar.Right>
-      </TopBar>
-      <div className={styles.Alert}>
-        👋 파티원분들과 반갑게 인사를 나눠보세요.
-        <br />
-        개인 정보 요구, 외부 채팅방으로 유도하는 경우 주의해주세요!
-      </div>
-      <div className={styles.MessageList}>
-        <div className={styles.MessageItem}>안녕하세요 친해져요~</div>
-        <div
-          className={classNames(styles.MessageItem, {
-            [styles.MyMessage]: true,
-          })}
-        >
-          안녕하세요 친해져요~
+    <div ref={wrapperRef}>
+      <div className={styles.Container} ref={containerRef}>
+        <TopBar>
+          <TopBar.Left back />
+          <TopBar.Center>친해지실 분 구해유</TopBar.Center>
+          <TopBar.Right close={false}>
+            <ChatRoomInfo />
+          </TopBar.Right>
+        </TopBar>
+        <div className={styles.Alert}>
+          👋 파티원분들과 반갑게 인사를 나눠보세요.
+          <br />
+          개인 정보 요구, 외부 채팅방으로 유도하는 경우 주의해주세요!
+        </div>
+        <div className={styles.MessageList}>
+          {Array.from({ length: 20 }).map(() => (
+            <div className={styles.MessageItem}>안녕하세요 친해져요~</div>
+          ))}
+          <div
+            className={classNames(styles.MessageItem, {
+              [styles.MyMessage]: true,
+            })}
+          >
+            안녕하세요 친해져요~
+          </div>
         </div>
       </div>
       <div className={styles.Bottom}>

6b804de65f51e3d249596b0892ab03007bd2ae1b
JunYoung Kim <twinjyjh2@gmail.com>
2024-07-23 19:49:56 +0900

    #101 refactor: ChatPage에서 사용되는 컴포넌트 파일로 분리

--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -1,10 +1,10 @@
 import Tabs from '@/pages/Home/components/Tabs.tsx'
 import styles from './ChatPage.module.scss'
-import Avatar from '@/components/Avatar.tsx'
 import TopBar from '@/components/NavBar/TopBar.tsx'
 import BottomBar from '@/components/NavBar/BottomBar.tsx'
-import emptyChat from '@/assets/empty_chat.png'
 import { useState } from 'react'
+import ChatItem from '@/pages/ChatPage/components/ChatItem.tsx'
+import EmptyChat from '@/pages/ChatPage/components/EmptyChat.tsx'
 
 type ChatType = 'party' | 'personal'
 
@@ -30,38 +30,3 @@ function ChatPage() {
 }
 
 export default ChatPage
-
-function ChatItem() {
-  return (
-    <div className={styles.ChatItem}>
-      <div className={styles.Left}>
-        <Avatar src={''} alt={'아바타'} className={styles.Avatar} />
-        <div className={styles.RoomInfo}>
-          <div className={styles.RoomTitle}>
-            <div className={styles.RoomName}>친해지실 분 구해유</div>
-            <div className={styles.Members}>9</div>
-          </div>
-          <div className={styles.LastMessage}>안녕하세요 친해져요~</div>
-        </div>
-      </div>
-      <div className={styles.Right}>
-        <div className={styles.LastChatTime}>오전 8:42</div>
-        <div className={styles.Chip}>12</div>
-      </div>
-    </div>
-  )
-}
-
-function EmptyChat() {
-  return (
-    <div className={styles.EmptyChat}>
-      <img src={emptyChat} alt={'empty-chat-img'} />
-      <div className={styles.EmptyChatText}>
-        참여중인 채팅이 없습니다.
-        <br />
-        파티에 참가하고 파티원과 대화를 나눠보세요!
-      </div>
-      <button className={styles.Button}>파티 목록 보기</button>
-    </div>
-  )
-}

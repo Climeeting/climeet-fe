6fa09c625215127de31dd99fc2903d86313b60e8
dev_hee <kheeyaa888@gmail.com>
2024-07-28 21:57:47 +0900

    #112 중복된 불필요한 코드 제거

--- src/pages/PartyDetailPage/components/PartyBottomButton.tsx
+++ src/pages/PartyDetailPage/components/PartyBottomButton.tsx
@@ -1,17 +1,12 @@
 import styles from './PartyBottomButton.module.scss'
 import Chip from '@/components/Chip'
 import AdditionalInfoDialog from '@/components/Dialog/AdditionalInfoDialog'
-import Dialog from '@/components/Dialog/Dialog'
 import LoginDialog from '@/components/Dialog/LoginDialog'
 import { post_party_$partyId_participate, usePartyDetail } from '@/services/party'
 import { useCheckAdditionalInfo, useIsLogin } from '@/services/user'
 import { useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
 
 export default function PartyBottomButton({ id }: { id?: string }) {
-  const navigate = useNavigate()
-  const location = useLocation()
-
   const { data: isLogin } = useIsLogin()
   const { data: partyData } = usePartyDetail(Number(id))
   const { data: checkAdditionalInfo } = useCheckAdditionalInfo()
@@ -42,14 +37,6 @@ export default function PartyBottomButton({ id }: { id?: string }) {
           </button>
         )}
       </Chip>
-
-      <Dialog open={openAlertLogin} onOpenChange={onOpenAlertLogin}>
-        <Dialog.Content onAction={() => navigate(`/login?redirect=${location.pathname}`)}>
-          로그인이 필요한 서비스입니다.
-          <br />
-          로그인 하시겠습니까?
-        </Dialog.Content>
-      </Dialog>
 
       <LoginDialog open={openAlertLogin} onOpenChange={onOpenAlertLogin} />
       <AdditionalInfoDialog

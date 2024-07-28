0cda779cee9c199f81597c6e67ef59005e8e934a
dev_hee <kheeyaa888@gmail.com>
2024-07-28 18:12:56 +0900

    #112 feat: 파티 참가시 추가정보 입력 받기

--- src/pages/PartyDetailPage/components/PartyBottomButton.tsx
+++ src/pages/PartyDetailPage/components/PartyBottomButton.tsx
@@ -1,11 +1,20 @@
 import styles from './PartyBottomButton.module.scss'
 import Chip from '@/components/Chip'
+import Dialog from '@/components/Dialog'
 import { post_party_$partyId_participate, usePartyDetail } from '@/services/party'
-import { useIsLogin } from '@/services/user'
+import { useCheckAdditionalInfo, useIsLogin } from '@/services/user'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function PartyBottomButton({ id }: { id?: string }) {
+  const navigate = useNavigate()
+
   const { data: isLogin } = useIsLogin()
   const { data: partyData } = usePartyDetail(Number(id))
+  const { data: checkAdditionalInfo } = useCheckAdditionalInfo()
+
+  const [openAlertLogin, onOpenAlertLogin] = useState(false)
+  const [openAlertAdditionalInfo, onOpenAlertAdditionalInfo] = useState(false)
 
   return (
     <>
@@ -15,7 +24,14 @@ export default function PartyBottomButton({ id }: { id?: string }) {
         ) : (
           <button
             onClick={async () => {
-              if (!isLogin) alert('로그인이 필요합니다.')
+              if (!isLogin) {
+                onOpenAlertLogin(true)
+                return
+              }
+              if (checkAdditionalInfo?.isTrue) {
+                onOpenAlertAdditionalInfo(true)
+                return
+              }
               await post_party_$partyId_participate(Number(id))
             }}
           >
@@ -23,6 +39,22 @@ export default function PartyBottomButton({ id }: { id?: string }) {
           </button>
         )}
       </Chip>
+
+      <Dialog open={openAlertLogin} onOpenChange={onOpenAlertLogin}>
+        <Dialog.Content onAction={() => navigate('/login')}>
+          로그인이 필요한 서비스입니다.
+          <br />
+          로그인 하시겠습니까?
+        </Dialog.Content>
+      </Dialog>
+
+      <Dialog open={openAlertAdditionalInfo} onOpenChange={onOpenAlertAdditionalInfo}>
+        <Dialog.Content onAction={() => navigate('/user/my/new')}>
+          파티에 참여하기 위해서는
+          <br />
+          추가정보 입력이 필요합니다.
+        </Dialog.Content>
+      </Dialog>
     </>
   )
 }

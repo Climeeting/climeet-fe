70afc36d54c7ce1e62c8e45ce05da82d44e685d5
dev_hee <kheeyaa888@gmail.com>
2024-07-30 23:05:42 +0900

    #123 aws-sdk TypeError

--- vite.config.ts
+++ vite.config.ts
@@ -50,5 +50,18 @@ export default defineConfig(({ mode }) => {
       // necessary for segment analytics lib to work
       global: {},
     },
+    resolve: {
+      alias: {
+        './runtimeConfig': './runtimeConfig.browser', // 모듈의 실제 위치를 브라우저 번들러에게 알려줌
+      },
+    },
+    build: {
+      commonjsOptions: {
+        include: [/node_modules/], // 번들에 포함시킬 모듈의 경로
+        extensions: ['.js', '.cjs'], // CommonJS 모듈로 간주할 파일의 확장자
+        strictRequires: true, // require 구문에 해당 모듈이 없을 경우 에러 발생
+        transformMixedEsModules: true, // import와 require문을 함께 사용하는 경우 이를 번들에 포함시키기 위함
+      },
+    },
   }
 })

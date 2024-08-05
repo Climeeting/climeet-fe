292f0ded9ad86fe78f8de8aaf0a008f8deee4199
piggmme <kheeyaa888@gmail.com>
2024-08-05 20:30:39 +0900

    #124 feat: 홈화면 마감임박 버튼 기능

--- src/pages/Home/components/RemoveFullButton.tsx
+++ src/pages/Home/components/RemoveFullButton.tsx
@@ -1,14 +1,18 @@
 import Icon from '@/components/Icon/Icon'
 import styles from './RemoveFullButton.module.scss'
 import { useState } from 'react'
+import { useFilterActions } from '../hooks/useFilterContext'
 
 export default function RemoveFullButton() {
+  const actions = useFilterActions()
   const [checked, setChecked] = useState(false)
+
   return (
     <button
       className={styles.Button}
       data-checked={checked}
       onClick={() => {
+        actions.status.toggle('신청하기')
         setChecked(!checked)
       }}
     >

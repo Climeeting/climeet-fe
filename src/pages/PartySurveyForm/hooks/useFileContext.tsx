23510344a2e2d581664ecbe5cc171ac05c2048a0
JunYoung Kim <twinjyjh2@gmail.com>
2024-09-10 00:54:42 +0900

    #153 feat: 파티 이미지 미리보기 기능 추가

---
+++ src/pages/PartySurveyForm/hooks/useFileContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react'
+
+const FileContext = createContext<File | null>(null)
+const FileActions = createContext<(file: File) => void>(() => {})
+
+export function FileProvider ({ children }: { children: React.ReactNode }) {
+  const [file, setFile] = useState<File | null>(null)
+
+  return (
+    <FileContext.Provider value={file}>
+      <FileActions.Provider value={setFile}>{children}</FileActions.Provider>
+    </FileContext.Provider>
+  )
+}
+
+export const useFileContext = () => {
+  return useContext(FileContext)
+}
+
+export const useFileActions = () => {
+  return useContext(FileActions)
+}

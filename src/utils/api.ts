405a7086f375798d3312076a68824c2fc7bf1de2
piggmme <kheeyaa888@gmail.com>
2024-09-10 17:29:58 +0900

    헤더에서 climeet 제거

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,9 +4,6 @@ import { isDevelop } from './env'
 const api = axios.create({
   withCredentials: true,
   baseURL: isDevelop ? undefined : import.meta.env.VITE_API_URL,
-  headers: {
-    climeet: import.meta.env.VITE_API_HEADERS_CLIMEET,
-  },
 })
 
 api.interceptors.response.use(

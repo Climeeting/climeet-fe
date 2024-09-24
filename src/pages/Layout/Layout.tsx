fb1f5b5eacb8ee1fc11b2778602455db5c5e61bd
dev_hee <kheeyaa888@gmail.com>
2024-09-24 22:53:10 +0900

    styles: desktop 에서 보기 편하게 ui 수정

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,14 +1,17 @@
 import { useKakaoMap } from '@/utils/useKakaoMap'
 import { Toasts } from '@/utils/useToast'
 import { Outlet, ScrollRestoration } from 'react-router-dom'
+import styles from './Layout.module.scss'
 
 export default function Layout () {
   const status = useKakaoMap()
 
   return (
-    <Toasts>
-      <ScrollRestoration />
-      {status && <Outlet />}
-    </Toasts>
+    <div className={styles.Layout}>
+      <Toasts>
+        <ScrollRestoration />
+        {status && <Outlet />}
+      </Toasts>
+    </div>
   )
 }

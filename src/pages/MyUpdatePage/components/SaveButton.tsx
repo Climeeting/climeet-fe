b572e4105e5eb8718ff6c770b06ad6b1caae07dc
piggmme <kheeyaa888@gmail.com>
2024-09-30 20:18:37 +0900

    #167 CLIM-61 마이페이지 수정버튼 클릭시 아무 반응 X

--- src/pages/MyUpdatePage/components/SaveButton.tsx
+++ src/pages/MyUpdatePage/components/SaveButton.tsx
@@ -8,12 +8,15 @@ import { useMyInfoFormContext } from '../hooks/useMyInfoForm'
 import styles from './SaveButton.module.scss'
 import { useFileContext } from '../hooks/useFileContext'
 import { uploadFileS3 } from '@/utils/s3'
+import useToast from '@/utils/useToast'
+import { useNavigate } from 'react-router-dom'
 
 export function SaveButton ({ onClick }: { onClick: () => void }) {
   const info = useMyInfoFormContext()
   const file = useFileContext()
   const { data } = useMyProfile()
   const myData = data ? new MyProfileBe2FeAdpter(data).adapt() : {}
+  const navigate = useNavigate()
 
   const getUserNewInfo = async () => {
     const newInfo = {
@@ -23,14 +26,25 @@ export function SaveButton ({ onClick }: { onClick: () => void }) {
     const diff = getDiff(newInfo, myData)
     return diff
   }
+  const toast = useToast()
 
   return (
     <button
       onClick={async () => {
         onClick?.()
 
-        const userNewInfo = await getUserNewInfo()
-        if (!isEmpty(userNewInfo)) await put_user_information(userNewInfo)
+        try {
+          const userNewInfo = await getUserNewInfo()
+          if (!isEmpty(userNewInfo)) await put_user_information(userNewInfo)
+          toast.add({
+            message: '프로필이 수정되었습니다.',
+          })
+          navigate(`/user/${data?.userId}`)
+        } catch {
+          toast.add({
+            message: '프로필 수정에 실패하였습니다.',
+          })
+        }
       }}
       className={styles.Button}
     >

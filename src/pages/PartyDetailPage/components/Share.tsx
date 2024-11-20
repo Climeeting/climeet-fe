2ae1542bb5212be45150426fdc3f0d5c81e9f9d8
piggmme <kheeyaa888@gmail.com>
2024-11-20 13:10:17 +0900

    fix: 파티 공유하기 동작 수정
    - share 메서드가 없을 때는 클립보드에 복사
    - share 할 때 파티 소개 text 는 제거

--- src/pages/PartyDetailPage/components/Share.tsx
+++ src/pages/PartyDetailPage/components/Share.tsx
@@ -1,16 +1,25 @@
 import Icon from '@/components/Icon/Icon.tsx'
 import { usePartyDetail } from '@/services/party'
+import useToast from '@/utils/useToast'
 
 export default function Share ({ id }: { id?: string }) {
   const { data: partyData } = usePartyDetail(Number(id))
+  const toast = useToast()
 
   return (
     <div onClick={() => {
-      window.navigator.share({
-        title: `클라이밋 | ${partyData?.partyName}`,
-        text: partyData?.partyDescription,
-        url: window.location.href,
-      })
+      if (!window.navigator.share) {
+        window.navigator.clipboard.writeText(window.location.href).then(() => {
+          toast.add({
+            message: '클립보드에 복사되었어요.',
+          })
+        })
+      } else {
+        window.navigator.share({
+          title: `클라이밋 | ${partyData?.partyName}`,
+          url: window.location.href,
+        })
+      }
     }}
     >
       <Icon icon='Share' size={24} />
